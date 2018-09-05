e6ee349973bb26da1a11f1090074a41344128c92
mateusz mazur <matmazur90@gmail.com>
2018-09-05 13:49:29 +0200

    added connection provider

---
+++ src/main/java/com/someapp/connection/ConnectionProvider.java
@@ -0,0 +1,30 @@
+package com.someapp.connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+
+    private DataSource dataSource;
+
+    public Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+
+    public DataSource getDataSource() {
+        if (dataSource == null) {
+            try {
+                Context initialContext = new InitialContext();
+                Context envContext = (Context) initialContext.lookup("java:comp/env");
+                dataSource = (DataSource) envContext.lookup("jdbc/mydb");
+            } catch (NamingException e) {
+                e.printStackTrace();
+            }
+        }
+        return dataSource;
+    }
+}
